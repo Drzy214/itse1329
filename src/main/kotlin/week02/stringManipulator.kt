78139ab7dee8bdc0f48e7368fb700224506bca65
Drzy214 <andrew.ruano963@gmail.com>
2022-09-13 10:12:01 -0500

    created folder and did 1 assignment

--- src/main/kotlin/week02/stringManipulator.kt
+++ src/main/kotlin/week02/stringManipulator.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>){
+    //user enters favorite city
     println("Enter your favorite city:")
     val city: String = readLine()!!
+    //displays the city in different ways
     println("Number of characters in your city: " + city.length)
     println("The name of your city in all caps: " + city.uppercase())
     println("The name of your city in all lower case: " + city.lowercase())
