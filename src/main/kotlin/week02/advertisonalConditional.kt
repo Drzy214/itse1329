a7cec777c7bd423ff8de95c2f7ea40ba00b622ce
Drzy214 <andrew.ruano963@gmail.com>
2022-11-01 08:39:26 -0500

    created folder and did 1 assignment

---
+++ src/main/kotlin/week02/advertisonalConditional.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var friendsCount: Int = 700
+    println("You have " + friendsCount + " Friends")
+    if(friendsCount > 500){
+        println("Adopt a Dog Today! ")
+    }else{
+        println(" Buy cat food at 20% off ")
+    }
+
+}
